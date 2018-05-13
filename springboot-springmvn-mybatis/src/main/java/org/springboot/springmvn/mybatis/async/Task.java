02cb34871993588c0484d19bc71ad5b571dc30cb
StoneSoftware <wu_firefox@163.com>
2018-05-13 12:24:54 +0800

    SpringBoot

---
+++ springboot-springmvn-mybatis/src/main/java/org/springboot/springmvn/mybatis/async/Task.java
@@ -0,0 +1,52 @@
+package org.springboot.springmvn.mybatis.async;
+
+import java.util.concurrent.Future;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Component;
+
+/**
+ * <实现异步执行> 
+ * <@Async 所修饰的函数不要定义为static,这样异步调用不会生效>
+ * 
+ * @author Administrator
+ *
+ */
+@Component
+public class Task {
+
+	@Async
+	public Future<String> task1() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.err.println("task1");
+		return new AsyncResult<String>("task1");
+	}
+
+	@Async
+	public Future<String> task2() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.err.println("task2");
+		return new AsyncResult<String>("task2");
+	}
+
+	@Async
+	public Future<String> task3() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.err.println("task3");
+		return new AsyncResult<String>("task3");
+	}
+
+}
